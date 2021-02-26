172a325e4066ed4e64dcd4ad7c5b4a9adb9bea99
Matthew Thompson <chatrat12@gmail.com>
2021-02-26 15:12:40 -0500

    Added Cutting Station with animation

---
+++ Assets/Scripts/Interactions/InteractableCuttingStation.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+public class InteractableCuttingStation : InteractableCounterTop
+{
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponentInChildren<Animator>();
+    }
+
+
+
+    public override void Interact(PlayerCharacter player)
+    {
+        _animator.SetTrigger("Interact");
+    }
+}
